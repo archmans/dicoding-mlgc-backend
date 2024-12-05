28119e2f1b914f9d51f5df2cefe4139b0fbec6ee
Muhamad Salman Hakim Alfarisi <13521010@std.stei.itb.ac.id>
2024-12-05 18:31:25 +0700

    chore: clean up console logs

--- src/config/model.js
+++ src/config/model.js
@@ -5,13 +5,9 @@ class Model {
     async loadModel() {
         try {
             const url = await Bucket.generatedUrl();
-            console.log('Loading model from URL:', url);
             const model = await tf.loadGraphModel(url);
-            console.log('Model loaded successfully');
-            console.log(model);
             return model;
         } catch (error) {
-            console.error('Error loading model:', error);
             throw error;
         }
     }
--- src/modules/predict/predict.repository.js
+++ src/modules/predict/predict.repository.js
@@ -6,15 +6,12 @@ class PredictRepository {
     }
 
     async savePrediction(predictionData) {
-        console.log('Saving prediction...');
         const { id, result, suggestion, createdAt } = predictionData;
         const docRef = this.db.collection('predictions').doc(id);
     
         try {
             await docRef.set({ id, result, suggestion, createdAt });
-            console.log(`Prediction saved with ID: ${id}`);
         } catch (error) {
-            console.error('Error saving prediction:', error);
             throw error;
         }
     }
