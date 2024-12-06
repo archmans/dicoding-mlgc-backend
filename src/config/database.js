870511facb4e0747142ccaf67b13feede12c5e75
archmans <13521010@std.stei.itb.ac.id.com>
2024-12-06 06:51:34 +0000

    fix: update get path service key

--- src/config/database.js
+++ src/config/database.js
@@ -1,22 +1,12 @@
-const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 const Firestore = require('@google-cloud/firestore');
+const path = require('path');
+const pathKey = path.resolve('./serviceKey.json');
 
 class Database {
     constructor() {
-        this.firestore = null;
-    }
-
-    async init() {
-        const secretClient = new SecretManagerServiceClient();
-        const [version] = await secretClient.accessSecretVersion({
-            name: 'projects/49760651936/secrets/serviceAccount/versions/latest',
-        });
-
-        const serviceKey = version.payload.data.toString('utf8');
-
         this.firestore = new Firestore({
             projectId: 'submissionmlgc-muhamadsalman',
-            credentials: JSON.parse(serviceKey),
+            keyFilename: pathKey,
         });
     }
 
@@ -25,11 +15,4 @@ class Database {
     }
 }
 
-const database = new Database();
-database.init().then(() => {
-    console.log('Firestore initialized');
-}).catch(err => {
-    console.error('Error initializing Firestore:', err);
-});
-
-module.exports = database;
+module.exports = new Database();
