7681d2c9222ed33d1ea88b6c6c393a6f848f7a5c
Muhamad Salman Hakim Alfarisi <13521010@std.stei.itb.ac.id>
2024-12-06 07:09:48 +0700

    feat: add secret manager for SA

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,22 @@
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 const Firestore = require('@google-cloud/firestore');
 
 class Database {
     constructor() {
+        this.firestore = null;
+    }
+
+    async init() {
+        const secretClient = new SecretManagerServiceClient();
+        const [version] = await secretClient.accessSecretVersion({
+            name: 'projects/49760651936/secrets/serviceAccount/versions/latest',
+        });
+
+        const serviceKey = version.payload.data.toString('utf8');
+
         this.firestore = new Firestore({
             projectId: 'submissionmlgc-muhamadsalman',
-            keyFilename: './serviceKey.json',
+            credentials: JSON.parse(serviceKey),
         });
     }
 
@@ -13,4 +25,11 @@ class Database {
     }
 }
 
-module.exports = new Database();
+const database = new Database();
+database.init().then(() => {
+    console.log('Firestore initialized');
+}).catch(err => {
+    console.error('Error initializing Firestore:', err);
+});
+
+module.exports = database;
